ac5a9a4b0c10db519edfd91bb74c6b48e5b25255
Denislav Kanev <denislavskanev@gmail.com>
2021-10-26 15:50:29 +0300

    fluent url builder

--- Portfolio/Services/StockService.cs
+++ Portfolio/Services/StockService.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using Casino.Common.Extensions;
+using Microsoft.AspNetCore.WebUtilities;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Portfolio.Models.Ticker;
 using RestSharp;
@@ -12,9 +14,21 @@ namespace Portfolio.Services
     public class StockService : IStockService
     {
         private string _apiKey { get; } = "OjHEzQV9oBrMAVO3Cml02vAiC1vBrS6O";
+        private string _baseUrl { get; } = "https://api.polygon.io/v2";
+        private string _apiKeyQueryParam { get; } = "apiKey";
+        private string _adjustedQueryParam { get; } = "adjusted";
         public TickerPreviousCloseOutput PreviousClose(string ticker)
         {
-            var client = new RestClient($"https://api.polygon.io/v2/aggs/ticker/" + ticker + "/prev?adjusted=true&apiKey=" + _apiKey);
+            var fluentUrlBuilder = new FluentUrlBuilder()
+                .BaseUrl(_baseUrl)
+                .AppendUrlSection("aggs")
+                .AppendUrlSection("ticker")
+                .AppendUrlSection(ticker)
+                .AppendUrlSection("prev")
+                .AppendQueryParam(_adjustedQueryParam, "true")
+                .AppendQueryParam(_apiKeyQueryParam, _apiKey);
+
+            var client = new RestClient(fluentUrlBuilder.GetUrl());
             var request = new RestRequest("", DataFormat.Json);
             var response = client.Get(request);
             var serialized = JsonConvert.DeserializeObject<TickerPreviousCloseOutput>(response.Content);
